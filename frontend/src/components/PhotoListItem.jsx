70b3f3f0ed72bcfaf45387716b81f702f9a227ad
Mandy Liu <manling512@gmail.com>
2023-10-16 12:45:12 -0600

    Create a component PhotoListItem for one specific photo, and refactor it to use props

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+const PhotoListItem = (props) => {
+  const { id, location, imageSource, username, profile } = props;
 
-const PhotoListItem = () => {
-  /* Insert React */
+  return (
+    <div className="photo-list__item">
+      <PhotoFavButton />
+      <img src={imageSource} alt={`Photo by ${username}`} className="photo-list__image" />
+      <img src={profile} alt={`Profile of ${username}`} className="photo-list__user-profile" />
+      <div className="photo-list__user-details">
+        <p className="photo-list__user-info">{username}</p>
+        <p className="photo-list__user-location">{location.city} {location.country}</p>
+      </div>
+    </div>
+  );
 };
 
 export default PhotoListItem;
